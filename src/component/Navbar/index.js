c0499a8cd276ad14757ab82a633238d9ae4db1ac
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-05-02 22:36:14 -0500

    add some lines in package.json

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -13,9 +14,18 @@ const Navbar = () => {
         <div className="navigation__background"></div>
         <nav className="navigation__nav">
             <ul className="navigation__list">
-                <li className="navigation__item"><a href="/bio" className="navigation__link">home</a></li>
-                <li className="navigation__item"><a href="/contact" className="navigation__link">contact</a></li>
-                <li className="navigation__item"><a href="/portfolio" className="navigation__link">portfolio</a></li>
+                <li className="navigation__item">
+                {/* <Link to="/bio" className="navigation__link">Home</Link> */}
+                    <a href="/bio" className="navigation__link">home</a>
+                </li>
+                <li className="navigation__item">
+                {/* <Link to="/contact" className="navigation__link">contact</Link> */}
+                    <a href="/contact" className="navigation__link">contact</a>
+                </li>
+                <li className="navigation__item">
+                {/* <Link to="/portfolio" className="navigation__link">portfolio</Link> */}
+                    <a href="/portfolio" className="navigation__link">portfolio</a>
+                </li>
             </ul>
         </nav>
     </div>
