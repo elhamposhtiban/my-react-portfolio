ac44adb6ccd786402638538a90c6044a6477398e
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-05-26 17:47:12 -0500

    adding favicon to the application and make some changes

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -16,15 +16,12 @@ const Navbar = () => {
             <ul className="navigation__list">
                 <li className="navigation__item">
                 <Link to="/bio" className="navigation__link">Home</Link>
-                    {/* <a href="/bio" className="navigation__link">home</a> */}
                 </li>
                 <li className="navigation__item">
                 <Link to="/contact" className="navigation__link">contact</Link>
-                    {/* <a href="/contact" className="navigation__link">contact</a> */}
                 </li>
                 <li className="navigation__item">
                 <Link to="/portfolio" className="navigation__link">portfolio</Link>
-                    {/* <a href="/portfolio" className="navigation__link">portfolio</a> */}
                 </li>
             </ul>
         </nav>
