4f3fd0e78d32fde574c9e43d154097b991121910
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-07-23 23:58:13 -0500

    changind links to a tags in navbar

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
+
+const [showPage, setShowPage] = useState(false)
+
+const showHandler = () => setShowPage(true)
+const closeHandler = () => setShowPage(false)
+
     return (
 
-    <div className="navigation">
+    <div className="navigation" showPage = {showPage}>
 
-        <input type="checkbox" className="navigation__checkbox" id="navi_toggle"/>
+        <input  type="checkbox" className="navigation__checkbox" id="navi_toggle"/>
         <label htmlFor="navi_toggle" className="navigation__button">
             <span className="navigation__icon">&nbsp;</span>
         </label>
         <div className="navigation__background"></div>
         <nav className="navigation__nav">
             <ul className="navigation__list">
-                <li className="navigation__item">
-                <Link to="/bio" className="navigation__link">Home</Link>
+
+            <li className="navigation__item">
+                <a href="/bio" className="navigation__link"   >Home</a>
+            </li>
+            <li className="navigation__item">
+                <a href="/contact" className="navigation__link" >contact</a>
+            </li>
+            <li className="navigation__item">
+                <a href="/portfolio" className="navigation__link"  >portfolio</a>
+            </li>
+                {/* <li className="navigation__item">
+                <Link to="/bio" className="navigation__link" onClick= {showHandler} >Home</Link>
                 </li>
                 <li className="navigation__item">
-                <Link to="/contact" className="navigation__link">contact</Link>
+                <Link to="/contact" className="navigation__link" onClick= {showHandler}>contact</Link>
                 </li>
                 <li className="navigation__item">
-                <Link to="/portfolio" className="navigation__link">portfolio</Link>
-                </li>
+                <Link to="/portfolio" className="navigation__link" onClick= {showHandler}>portfolio</Link>
+                </li> */}
             </ul>
         </nav>
     </div>
