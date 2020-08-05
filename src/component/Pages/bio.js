56020f908fe283811c0979ed046ca17cdf7b55a4
Byron <byron.miller@gmail.com>
2020-08-04 23:17:19 -0400

    Grammar/Typo Updates && 'About Me Card' Spacing

--- src/component/Pages/bio.js
+++ src/component/Pages/bio.js
@@ -23,37 +23,44 @@ const Bio = () => {
                 <section className="section-stories">
 
                     <div className="row about-card-container">
-  
-                            <div className="about-card" id="about-me">
+
+                        <div className="about-card" id="about-me">
                             <div className="about-card__side about-card__side--front">
                                 <div className="about-card__picture about-card__picture--1">
-                                    
+
                                 </div>
-                            <h4 className="about-card__heading">
-                                <figure className="story__shape">
-                                    <img src={ImageProfile} alt="elham poshtiban" className="story__img"/>
-                                </figure>
-                            </h4>
+                                <h4 className="about-card__heading">
+                                    <figure className="story__shape">
+                                        <img src={ImageProfile} alt="elham poshtiban" className="story__img" />
+                                    </figure>
+                                </h4>
                                 <div className="about-card__details">
-                                        <div className="heading-about">
+                                    <div className="heading-about">
 
-                                            <h1 className="heading-about__main">
-                                                ELHAM POSHTIBAN
+                                        <h1 className="heading-about__main">
+                                            ELHAM POSHTIBAN
                                             </h1>
-                                            <hr></hr>
-                                            <p className="heading-about__sub">  WEB DEVELOPER | CREATIVE | ambitious | Innovator </p>
+                                        <hr></hr>
+                                        <p className="heading-about__sub">  WEB DEVELOPER | CREATIVE | ambitious | Innovator </p>
 
-                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="about-card__side about-card__side--back about-card__side--back">
                                 <div className="about-card__cta">
-                                <p> ABOUT ME</p>
-                                <hr></hr>
-                                <p> Passionate, detail-oriented Full Stack Web Developer with a 
+                                    <p> ABOUT ME</p>
+                                    <hr></hr>
+                                    <p> Passionate and detail-oriented
+                                        <br />
+                                    Full Stack Web Developer with a
                                     certificate from UT Austin coding Boot-camp and degree in Software Engineering.
+                                    <br />
+                                        <br />
+
                                     Highly enthusiastic about coding and learning new materials.
-                                    Prepared to excel under stressful situations and maintain a sense of humor to 
+                                                                        <br />
+                                        <br />
+                                    Prepared to excel under stressful situations and maintain a sense of humor to
                                     collaborate with a team in a productive, positive manner across fast-paced settings.
                                 </p>
                                 </div>
@@ -62,41 +69,41 @@ const Bio = () => {
 
                     </div>
 
-                    
-                     <hr></hr>
+
+                    <hr></hr>
                     <div className="row">
                         <div className="col-1-of-3 skill-section">
-                            <img src={ImageFrontEnd} alt="front end pic"  className=" skill-icon animated fadeIn"/>
+                            <img src={ImageFrontEnd} alt="front end pic" className=" skill-icon animated fadeIn" />
                             <h5 className="heading-third">Front-end</h5>
                             <p className="skill-typo">
                                 I am most interest in Front end technologies.
                                 My front-end skills include HTML, CSS, and JavaScript Languages.
-                                besides of these languages, I am skilled in some libraries Like jQuery, Bootstrap and SCSS.  
-                                Also, I am Using some Frameworks such as React, Handlebars, Redux to create  
+                                Besides these languages, I am skilled in some libraries like jQuery, Bootstrap and SCSS.
+                                Also, I am Using some Frameworks such as React, Handlebars, and Redux to create
                                 user-Friendly web applications.
                             </p>
                         </div>
 
                         <div className="col-1-of-3 skill-section">
-                            <img src={ImageBackEnd} alt="front end pic"  className=" skill-icon animated fadeIn"/>
+                            <img src={ImageBackEnd} alt="front end pic" className=" skill-icon animated fadeIn" />
                             <h5 className="heading-third">Back-end</h5>
-                            <p  className="skill-typo">
-                                The technology and technical skills in my back-end toolbelt include: 
-                                Node, Express, NPM, REST and User authentication as well as the MongoDB, 
+                            <p className="skill-typo">
+                                The technology and technical skills in my back-end toolbelt include:
+                                Node, Express, NPM, REST and User authentication as well as MongoDB,
                                 MySQL databases and Firebase. I am very skilled at connecting everything behind the scenes.
                             </p>
                         </div>
 
                         <div className="col-1-of-3 skill-section">
-                            <img src={ImageFullStack} alt="front end pic"  className=" skill-icon animated fadeIn"/>
+                            <img src={ImageFullStack} alt="front end pic" className=" skill-icon animated fadeIn" />
                             <h5 className="heading-third">FULl-Stack</h5>
-                            <p  className="skill-typo">
-                                My skill set and experience extends to full-stack applications, which means creating and
+                            <p className="skill-typo">
+                                My skill set and experience extend to full-stack applications, which means creating and
                                 developing a fully operational website, server and all, from scratch. I have developed applications
                                 using the MERN stack and RESTful routes.
                             </p>
                         </div>
-                    </div> 
+                    </div>
 
                 </section>
             </main>
@@ -119,8 +126,8 @@ export default Bio
 <hr /> */}
 
 
-    
-                    {/* <div className="story" id="about-me">
+
+{/* <div className="story" id="about-me">
                             <figure className="story__shape">
                                 <img src={ImageProfile} alt="elham poshtiban" className="story__img"/>
                             </figure>
